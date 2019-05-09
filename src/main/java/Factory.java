b5a6cc57fd00dcc928ae85abf1949205bd53f656
MaximeSchlegel <maxime.schlegel.83@gmail.com>
2019-05-09 09:25:16 +0200

    Initial commit

---
+++ src/main/java/Factory.java
@@ -0,0 +1,30 @@
+
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Factory {
+    public static boolean isValid(OntModel model) {
+        return model.validate().isValid();
+    }
+
+    public static Model loadModel(String inputFileName) {
+        OntModel model = ModelFactory.createOntologyModel(new OntModelSpec(OntModelSpec.OWL_MEM));
+        ClassLoader classLoader = Factory.class.getClassLoader();
+        InputStream in = null;
+        try {
+            in = new FileInputStream(new File(classLoader.getResource(inputFileName).getFile()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        // read the OWL file
+        model.read(in, null);
+        return model;
+    }
+}
